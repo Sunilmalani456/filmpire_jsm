05aec27066bd106e79562af56cecd2640424d25b
sunil_malani <sunilmalani456@gmail.com>
2022-08-12 22:21:56 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Sunil Malani 0_0: </h1>
+    </div>
+  );
+}
+
+export default App;
